5f01584f1c0d6e8e43c6e2dabe25d86c2b925b8f
leocavalcante <lc@leocavalcante.com>
2020-03-22 16:29:11 -0300

    Paises lingua portuguesa, closes #1

--- timeseries/extract.php
+++ timeseries/extract.php
@@ -1,20 +1,41 @@
 <?php declare(strict_types=1);
 
+$countries = [
+    'Brazil',
+    'Angola',
+    'Mozambique',
+    'Portugal',
+    'Guinea-Bissau',
+    'East Timor',
+    'Equatorial Guinea3',
+    'Macau2',
+    'Cape Verde',
+    'São Tomé and Príncipe',
+];
+
 $source = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv';
 $handle = fopen($source, 'r');
 $header = [];
-$output = fopen('casos-confirmados-brasil.csv', 'w');
+
+$outputs = array_map(function (string $country): array {
+    return [$country, fopen("casos-confirmados-$country.csv", 'w')];
+}, $countries);
+
 
 while ($row = fgetcsv($handle)) {
     if (empty($header)) {
         $header = $row;
-        fputcsv($output, $header);
+
+        foreach ($outputs as [$country, $output]) {
+            fputcsv($output, $header);
+        }
+
         continue;
     }
 
-    $country = $row[1];
-
-    if ($country === 'Brazil') {
-        fputcsv($output, $row);
+    foreach ($outputs as [$country, $output]) {
+        if ($row[1] === $country) {
+            fputcsv($output, $row);
+        }
     }
 }
